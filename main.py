a87a337761ce3065ef06138b145e506dcd5bec28
Simakkk <n_simatov@mail.ru>
2025-02-06 04:33:37 +0300

    Linter

--- main.py
+++ main.py
@@ -3,35 +3,37 @@ import moderngl_window as mglw
 
 class App(mglw.WindowConfig):
     window_size = 1600, 900
-    resource_dir = 'programs'
+    resource_dir = "programs"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.quad = mglw.geometry.quad_fs()
-        self.program = self.load_program(vertex_shader='vertex.glsl', fragment_shader='fragment.glsl')
+        self.program = self.load_program(
+            vertex_shader="vertex.glsl", fragment_shader="fragment.glsl"
+        )
         self.u_scroll = 3.0
 
-        self.texture1 = self.load_texture_2d('../test0.png')
-        self.texture2 = self.load_texture_2d('../hex.png')
-        self.texture3 = self.load_texture_2d('../white_marble1.png')
-        self.texture4 = self.load_texture_2d('../texture3.jpg')
-        self.texture5 = self.load_texture_2d('../black_marble1.png')
-        self.texture6 = self.load_texture_2d('../green_marble1.png')
-        self.texture7 = self.load_texture_2d('../height3.png')
+        self.texture1 = self.load_texture_2d("../test0.png")
+        self.texture2 = self.load_texture_2d("../hex.png")
+        self.texture3 = self.load_texture_2d("../white_marble1.png")
+        self.texture4 = self.load_texture_2d("../texture3.jpg")
+        self.texture5 = self.load_texture_2d("../black_marble1.png")
+        self.texture6 = self.load_texture_2d("../green_marble1.png")
+        self.texture7 = self.load_texture_2d("../height3.png")
 
-        self.program['u_scroll'] = self.u_scroll
-        self.program['u_resolution'] = self.window_size
-        self.program['u_texture1'] = 1
-        self.program['u_texture2'] = 2
-        self.program['u_texture3'] = 3
-        self.program['u_texture4'] = 4
-        self.program['u_texture5'] = 5
-        self.program['u_texture6'] = 6
-        self.program['u_texture7'] = 7
+        self.program["u_scroll"] = self.u_scroll
+        self.program["u_resolution"] = self.window_size
+        self.program["u_texture1"] = 1
+        self.program["u_texture2"] = 2
+        self.program["u_texture3"] = 3
+        self.program["u_texture4"] = 4
+        self.program["u_texture5"] = 5
+        self.program["u_texture6"] = 6
+        self.program["u_texture7"] = 7
 
     def render(self, time, frame_time):
         self.ctx.clear()
-        self.program['u_time'] = time
+        self.program["u_time"] = time
         self.texture1.use(location=1)
         self.texture2.use(location=2)
         self.texture3.use(location=3)
@@ -42,12 +44,12 @@ class App(mglw.WindowConfig):
         self.quad.render(self.program)
 
     def mouse_position_event(self, x, y, dx, dy):
-        self.program['u_mouse'] = (x, y)
+        self.program["u_mouse"] = (x, y)
 
     def mouse_scroll_event(self, x_offset, y_offset):
         self.u_scroll = max(1.0, self.u_scroll + y_offset)
-        self.program['u_scroll'] = self.u_scroll
+        self.program["u_scroll"] = self.u_scroll
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mglw.run_window_config(App)
